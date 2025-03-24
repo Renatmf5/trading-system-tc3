25b72d4cc24c31f04e9a50baeb8d41fb3fe430f7
renatmf5 <renato_mf5@hotmail.com>
2025-03-23 21:59:10 -0300

    test

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 DIR="/home/ec2-user/trading-app"
-echo "AfterInstall: Instalando dependências"
+echo "AfterInstall: Iniciando script" | tee -a /home/ec2-user/trading-app/deploy.log
 sudo chown -R ec2-user:ec2-user ${DIR}
-cd ${DIR}
+cd ${DIR} || { echo "Erro: Não foi possível acessar o diretório ${DIR}"; exit 1; }
 
 # Criar ambiente virtual e instalar dependências
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+echo "AfterInstall: Criando ambiente virtual" | tee -a /home/ec2-user/trading-app/deploy.log
+python3 -m venv venv || { echo "Erro: Falha ao criar o ambiente virtual"; exit 1; }
+source venv/bin/activate || { echo "Erro: Falha ao ativar o ambiente virtual"; exit 1; | tee -a /home/ec2-user/trading-app/deploy.log }
+
+echo "AfterInstall: Instalando dependências"
+pip install -r requirements.txt || { echo "Erro: Falha ao instalar dependências"; exit 1; }
 
 # Criar arquivo de serviço systemd para a aplicação
+echo "AfterInstall: Criando arquivo de serviço systemd" | tee -a /home/ec2-user/trading-app/deploy.log
 sudo bash -c 'cat <<EOF > /etc/systemd/system/trading-app.service
 [Unit]
 Description=First trading Application
@@ -27,5 +31,5 @@ WantedBy=multi-user.target
 EOF'
 
 # Recarregar systemd para aplicar as mudanças
-sudo systemctl daemon-reload
-
+echo "AfterInstall: Recarregando systemd"
+sudo systemctl daemon-reload || { echo "Erro: Falha ao recarregar o systemd"; exit 1; }
